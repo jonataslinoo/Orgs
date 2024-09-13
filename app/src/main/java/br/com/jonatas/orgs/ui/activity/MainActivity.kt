db78c6214751b9d2829892e180679d7aee16cf2c
jonataslinoo <jonatas.linoo@gmail.com>
2024-09-12 21:48:11 -0300

    Adicionando classe de adapter e layout do recyclerview para apresentar a lista de produtos.

--- app/src/main/java/br/com/jonatas/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/jonatas/orgs/ui/activity/MainActivity.kt
@@ -2,12 +2,39 @@ package br.com.jonatas.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import br.com.jonatas.orgs.R
+import br.com.jonatas.orgs.model.Produto
+import br.com.jonatas.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ListaProdutosAdapter(
+            context = this, produtos = listOf(
+                Produto(
+                    nome = "Cesta de Frutas",
+                    descricao = "Laranja, manga e maçã",
+                    valor = BigDecimal(
+                        "19.99"
+                    )
+                ),
+                Produto(
+                    nome = "Cesta de Frutas 2",
+                    descricao = "Laranja, manga e uva",
+                    valor = BigDecimal("9.99")
+                ),
+                Produto(
+                    nome = "Cesta de Frutas 3",
+                    descricao = "Laranja, manga e banana",
+                    valor = BigDecimal("29.99")
+                )
+            )
+        )
     }
 }
