d9da30f2cd8abd0a83c0db6d99af6b26f0b814af
jonataslinoo <jonatas.linoo@gmail.com>
2024-09-12 22:08:47 -0300

    Adicionando activity de formulario produto

---
+++ app/src/main/java/br/com/jonatas/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,12 @@
+package br.com.jonatas.orgs.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.jonatas.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_produto)
+    }
+}
