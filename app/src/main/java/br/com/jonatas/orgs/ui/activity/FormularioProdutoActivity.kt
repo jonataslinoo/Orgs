7930ab5302b280a1cc58933e55578811f8e9df57
jonataslinoo <jonatas.linoo@gmail.com>
2024-09-13 23:02:00 -0300

    Adicionando campos para preenchimento do formulario produto e salvar.

--- app/src/main/java/br/com/jonatas/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/jonatas/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,12 +1,39 @@
 package br.com.jonatas.orgs.ui.activity
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import br.com.jonatas.orgs.R
+import br.com.jonatas.orgs.model.Produto
+import java.math.BigDecimal
 
-class FormularioProdutoActivity : AppCompatActivity() {
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_formulario_produto)
+
+        val campoNome = findViewById<EditText>(R.id.nome)
+        val nome = campoNome.text.toString()
+        val campoDescricao = findViewById<EditText>(R.id.descricao)
+        val descricao = campoDescricao.text.toString()
+        val campoValor = findViewById<EditText>(R.id.valor)
+        val valorEmTexto = campoValor.text.toString()
+        val valor = if (valorEmTexto.isBlank()) {
+            BigDecimal.ZERO
+        } else {
+            BigDecimal(valorEmTexto)
+        }
+        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+
+        val produtoNovo = Produto(
+            nome = nome,
+            descricao = descricao,
+            valor = valor
+        )
+
+        botaoSalvar.setOnClickListener {
+            Log.i("PRODUTO", "onCreate: $produtoNovo")
+        }
     }
 }
